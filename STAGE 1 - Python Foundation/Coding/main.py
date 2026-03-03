644bdc36e1cf464d0d4c28dbc59863fb5bb174ed
tushar patil <patilt035@gmail.com>
2026-03-03 14:40:13 +0530

    created Notes folder for Python

---
+++ STAGE 1 - Python Foundation/Coding/main.py
@@ -0,0 +1,34 @@
+# print("Hello World")
+
+# x = 10
+# print(x)
+# print(type(x))
+
+# name = 'Tushar'
+
+# print(name)
+# print(name[0:3])   
+# print(len(name))
+
+# numbers = [1, "2", 3, 4]
+# print(numbers)
+# print(type(numbers))
+
+
+# numbers.append(5)
+# print(numbers)
+
+# numbers[0] = 100
+# print(numbers)
+
+# coordinates[0] = 20 assignment not allowed for tuple
+# print(coordinates.count(2)) 
+# print(coordinates[0])
+# print(coordinates.index(100))
+# print(coordinates)
+
+coordinates = (10, 20,10)
+coordinates2 = (10, '20',10)
+
+print(coordinates2)
+print(type(coordinates))
