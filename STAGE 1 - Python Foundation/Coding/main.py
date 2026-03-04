a7d7f2552ebb415dc8fb975d9689bd1b784cb32d
tushar patil <patilt035@gmail.com>
2026-03-04 11:00:26 +0530

    completed datatypes

--- STAGE 1 - Python Foundation/Coding/main.py
+++ STAGE 1 - Python Foundation/Coding/main.py
@@ -27,8 +27,58 @@
 # print(coordinates.index(100))
 # print(coordinates)
 
-coordinates = (10, 20,10)
-coordinates2 = (10, '20',10)
+# coordinates = (10, 20,10)
+# coordinates2 = (10, '20',10)
 
-print(coordinates2)
-print(type(coordinates))
+# print(coordinates2)
+# print(type(coordinates))
+
+# nums = {1, 2, 3, 3, 4}
+
+# print(nums);
+
+# person = {
+#     "name": "Tushar",
+#     "age": 25,
+#     "city": "Latur"
+# }
+
+# print(person)
+# print(person["name"])
+
+# print(str(1) + " hello")
+# data = None
+# print(data)
+# print(type(data))
+
+# Hello "Tushar"
+# print("hello \"Tushar\"")
+# print('hello "Tushar"')
+
+# print(1+1.0)
+# print(4%3)
+
+# print(set([1, 2, 3, 3, 4])) 
+
+# set is unordered
+# numbers = {10, 20, 30}
+# numbers = {10, 20, 30}
+
+# print(numbers)
+
+# my_list = [3, 1, 2, 1]
+# my_set = {3, 1, 2, 1}
+
+# print("List:", my_list)
+# print("Set:", my_set)
+
+# print("HEllo")
+
+# indentation matters
+# if 1<2 :
+#     print("yes")
+# else:
+#     print("no")
+    
+# print("without in else")
+# print("without")
