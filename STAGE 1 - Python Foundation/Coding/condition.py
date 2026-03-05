83022265788df72f79533feb6318f56b1507b407
tushar patil <patilt035@gmail.com>
2026-03-05 11:36:06 +0530

    added git ignore file

---
+++ STAGE 1 - Python Foundation/Coding/condition.py
@@ -0,0 +1,58 @@
+# age = 18
+
+# if age >= 18:
+#     print("You are an adult")
+# elif age == 12:
+#     print("12")    
+# else:
+#     print("minor")
+
+
+# if age >18:
+#     if age>27:
+#         print("greater than 27") 
+#     else:
+#         print("greater than 18 but less than 27")
+# else:
+#     print("less than or equal to 18")
+
+# # two loops 
+# # for and while loop
+# # list
+# names = ["A", "B", "C"] 
+
+# for name in names:
+#         print(name)
+
+# # tuple 
+# names = ("A", "B", "C")
+
+# for name in names:
+#      print(name)
+
+# # set 
+# names = ("A", "A", "B", "C")
+
+# for name in names:
+#      print(name)     
+
+# dectionary
+# person = {
+#     "firstname": "Tushar",
+#     "age" : "18"
+# }    
+
+# for key in person:
+#      print(key)    
+ 
+# for key in person:
+#      print(person[key])       
+   
+# for key, value in person.items():
+#      print(key, value)   
+
+# for key in person.keys():
+#      print(key) 
+
+# for value in person.values():
+#      print(value)      
